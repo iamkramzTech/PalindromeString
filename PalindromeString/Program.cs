b205f903653047cb78bf7b5edf2ca850e8fab0e3
iamkramzTech <bernaldezm88@yahoo.com>
2022-08-13 22:50:16 +0800

    Add project files.

---
+++ PalindromeString/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+using System;
+
+namespace PalindromeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Green;
+            Write("Enter a String to Check if it is a Palindrome: ");
+            string text = ReadLine();
+            char[] strChar = text.ToCharArray();
+
+            //using Array.Reverse Method to reverse array of characters
+            Array.Reverse(strChar);
+
+            //Create a sting from strChar array
+            string reverse = new string(strChar);
+            WriteLine();
+            if(text.Equals(reverse,StringComparison.OrdinalIgnoreCase))
+            {
+                WriteLine($"{reverse} is a Palindrome");
+            }
+            else
+            {
+                WriteLine($"{reverse} is not a Palindrome");
+            }
+            ReadKey();
+        }
+    }
+}
